3aa4f554c8adaf055b2b07c05acc873de333d5ce
Gustavo <arieivovatsug@gmail.com>
2024-08-12 23:23:41 -0300

    added work items

--- tasks.py
+++ tasks.py
@@ -2,33 +2,34 @@ from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 from robocorp.tasks import task
+from robocorp.workitems import inputs
 from zoneinfo import ZoneInfo
 
 from scraper import LATimesScraper
 
 
 @task
-def minimal_task():
-    print("oi")
-    search_query = "Golf"
-    category = "sports"
-    n_months = 1
+def scrape_LA_times():
+    for inp in inputs:
+        search_query = inp.payload.get("search_query")
+        category = inp.payload.get("category")
+        n_months = int(inp.payload.get("months"))
 
-    n_months = max(n_months, 1)
+        n_months = max(n_months, 1)
 
-    scraper = LATimesScraper()
-    scraper.set_webdriver()
-    scraper.open_homepage()
-    scraper.search_for(search_query)
-    scraper.filter_by_category(category)
+        scraper = LATimesScraper()
+        scraper.set_webdriver()
+        scraper.open_homepage()
+        scraper.search_for(search_query)
+        scraper.filter_by_category(category)
 
-    current_month = datetime.now(ZoneInfo("UTC")).date().replace(day=1)
-    min_date = current_month - relativedelta(months=(n_months - 1))
-    news = scraper.get_news(search_query, min_date)
+        current_month = datetime.now(ZoneInfo("UTC")).date().replace(day=1)
+        min_date = current_month - relativedelta(months=(n_months - 1))
+        news = scraper.get_news(search_query, min_date)
 
-    for n in news:
-        print(n)
+        for n in news:
+            print(n)
 
-    print(f"Found {len(news)} articles")
+        print(f"Found {len(news)} articles")
 
-    scraper.driver_quit()
+        scraper.driver_quit()
